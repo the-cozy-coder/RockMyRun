07e042ac3d8b664c27239c5322fb9bd4551ec3b7
Krista Smith <kristaj@gmail.com>
2026-08-10 09:39:43 -0400

    fixed typo - I can't spell

--- app/models/User.py
+++ app/models/User.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 class User(db.Model):
 
     __tablename__ = "user"
-    id = db.Column(db.Integar, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     spotify_id = db.Column(db.String, unique=True, nullable=False)
     display_name = db.Column(db.String)
     access_token = db.Column(db.String)
