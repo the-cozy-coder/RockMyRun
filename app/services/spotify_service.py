9571994d7ab773e8af10c04989aae6eafe48127e
Krista Smith <kristaj@gmail.com>
2026-08-14 11:42:22 -0400

    debugging logging

--- app/services/spotify_service.py
+++ app/services/spotify_service.py
@@ -30,11 +30,17 @@ def get_playlist_songs(user_id, pl_id):
 
 
 def get_spotify_track_id(user_id, title, artist, spotify=None):
+    current_app.logger.info(
+        f"Searching Spotify: title='{title}', artist='{artist}'"
+    )
     user = User.query.get(user_id)
     current_app.logger.info(f"user_id={user_id}")
     current_app.logger.info(f"user={user}")
     if spotify is None:
         access_token = get_valid_access_token(user)
+        current_app.logger.info(
+            f"Token length={len(access_token)}"
+        )
         spotify = SpotifyClient(access_token)
     return spotify.search_spotify_track_id(title, artist, limit = 1), spotify
         
