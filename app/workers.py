2ca45946169fd7871b89bb60201044a771d5e7ee
Krista Smith <kristaj@gmail.com>
2026-08-15 15:02:53 -0400

    fix missing seed bug

--- app/workers.py
+++ app/workers.py
@@ -32,20 +32,19 @@ def get_recommendations(app, job_id, seeds, duration: int = 30, isVibe = False):
     with app.app_context():
         jobs[job_id]['message'] = "Processing song requests"
         seed_ids, search_crit = process_seeds(jobs[job_id]['user_id'], seeds)
-        if len(seed_ids) == 0:
-            if jobs[job_id]['user_id'] is None:
-                raise Exception("""<h2>None of your seed tracks were found in the local database.</h2>\n
-                <h3>Troubleshooting Recommendations:</h3>
-                <ol>
-                <li>Log into Spotify to take advantage of their much larger database.<br>
-                    This will also add the song info to our database for future use!</li>
-                <li>Please check your spelling and try again.  <br>
-                    RockMyRun does not currently support fuzzy matching</li>
-                </ol>
-                """)
-            else:
-                raise Exception("""None of your seed tracks were found in Spotify.\n
-                                Please check your spelling and try again.""")
+        if len(seed_ids) == 0 and jobs[job_id]['user_id'] is None:
+            raise Exception("""<h2>None of your seed tracks were found in the local database.</h2>\n
+            <h3>Troubleshooting Recommendations:</h3>
+            <ol>
+            <li>Log into Spotify to take advantage of their much larger database.<br>
+                This will also add the song info to our database for future use!</li>
+            <li>Please check your spelling and try again.  <br>
+                RockMyRun does not currently support fuzzy matching</li>
+            </ol>
+            """)
+            # else:
+            #     raise Exception("""None of your seed tracks were found in Spotify.\n
+            #                     Please check your spelling and try again.""")
         num_recommendations = duration // 2
         seed_recommendations = KNN_recommendations(seed_ids, 
                                                     k=num_recommendations)
@@ -69,8 +68,6 @@ def start_search_pipeline(
     seeds
 ):
     app = current_app._get_current_object()
-    if len(seeds) == 0:
-        seeds = 'Thunder Road - Bruce Springstein'
 
     thread = threading.Thread(
         target=run_search_pipeline,
@@ -130,9 +127,7 @@ def start_vibe_pipeline(
     vibe_profile
 ):
     app = current_app._get_current_object()
-    # We need some seed to get started and this is a good song.
-    if len(seeds) == 0:
-        seeds = 'Thunder Road - Bruce Springstein'
+
 
     thread = threading.Thread(
         target=run_vibe_pipeline,
