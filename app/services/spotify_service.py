27330a31358119e3c260ab456f0efd9a90dfbdb5
Krista Smith <kristaj@gmail.com>
2026-08-17 07:55:36 -0400

    remove unnecessary code

--- app/services/spotify_service.py
+++ app/services/spotify_service.py
@@ -30,23 +30,6 @@ def get_playlist_songs(user_id, pl_id):
     return spotify.get_playlist_songs(pl_id)
 
 
-# def get_spotify_track_id(user_id, title, artist, spotify=None):
-#     current_app.logger.info(
-#         f"Searching Spotify: title='{title}', artist='{artist}'"
-#     )
-#     user = User.query.get(user_id)
-#     if user is None:
-#         return [], None
-#     current_app.logger.info(f"user_id={user_id}")
-#     current_app.logger.info(f"user={user}")
-#     if spotify is None:
-#         access_token = get_valid_access_token(user)
-#         current_app.logger.info(
-#             f"Token length={len(access_token)}"
-#         )
-#         spotify = SpotifyClient(access_token)
-#     return spotify.search_spotify_track_id(title, artist, limit = 5), spotify
-
 
 def get_spotify_track_id(title, artist, limit=5):
         current_app.logger.info(f"searching spotify without login for {title} - {artist}")
